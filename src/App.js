6d0d1f834cd3d61d6562691af1c87ce7e990d65b
venki180497 <vengat180497@gmail.com>
2023-07-03 13:39:44 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
 import './App.css';
-
+import axios from 'axios'
 function App() {
+  const [movies,setMovies]=useState([])
+useEffect(()=>{
+getallmovie()
+},[])
+const getallmovie=async()=>{
+  const response=await axios({url:"http://localhost:4000/movie/",method:"GET"})
+  setMovies(response.data)
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {
+      movies[0]&&movies.map((item)=>{
+        return(
+          <div className='card'>
+          <h4 className='title'>
+          Movie Name: {item?.name}
+          </h4>
+          <p className='rating'>Rating: {item.rating}</p>
+          <p className='releasedate'>Releaseddate: {item.releaseddate} </p>
+          </div>
+        )
+
+      })
+     }
     </div>
   );
 }
